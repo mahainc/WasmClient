ac3bdf64a18c5ffedd5a96ccbbbb87f3fbac39f4
Thanh Hai Khong <haikt@vtn-global.com>
2026-03-27 09:14:29 +0700

    chore: bump flow-kit to 1.2.5

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/haikt-vtn/flow-kit.git",
-            from: "1.2.4"
+            from: "1.2.5"
         ),
     ],
     targets: [
